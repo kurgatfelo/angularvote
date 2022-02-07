e120e62025586e60c20cb854dc50da661abdf300
felo <felix.kurgat@student.moringaschool.com>
2022-02-07 10:35:45 +0300

    component templateURL

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote';
 @Component({
+  selector: 'app-quote',
   
-  templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
